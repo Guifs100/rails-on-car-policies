e67e6abc96405b8f76377db429f596ceb22e89e4
guifs100 <guifs100@gmail.com>
2024-05-06 16:31:07 -0300

    Criando status para policy

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -8,11 +8,13 @@ class Policy < ApplicationRecord
   validates :vehicle_id, presence: true
   validate :invalid_dates
 
+  enum :status, { pending: 0, expired: 5, active: 10 }, default: :pending
+
+  private
+
   def invalid_dates
     errors.add(:date_issue, 'invalid date') if invalid_date?
   end
-
-  private
 
   def invalid_date?
     date_issue > policy_expiration
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_27_141628) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_06_190944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_27_141628) do
     t.bigint "vehicle_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["insured_id"], name: "index_policies_on_insured_id"
     t.index ["vehicle_id"], name: "index_policies_on_vehicle_id"
   end
